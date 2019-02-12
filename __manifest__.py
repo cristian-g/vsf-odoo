2ca5f0bc85a6121e618c6a742224c63939b621f4
Cristian González <hello@cristiangonzalez.com>
2019-02-12 23:00:12 +0100

    Create __manifest__.py
    Refering #1

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'RESTful API',
+    'version': '0.0.1',
+    'category': 'API',
+    'author': 'Cristian González Ruz',
+    'website': 'http://cristiangonzalez.com',
+    'summary': 'RESTful API',
+    'support': 'hello@cristiangonzalez.com',
+    'description': """
+RESTful API
+===========
+See https://github.com/cristian-g/restful
+""",
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'data/ir_config_param.xml',
+        'views/ir_model.xml',
+        'views/res_users.xml',
+        'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/main_screenshot.png'],
+    'license': 'unknown',
+    'installable': True,
+    'auto_install': False,
+}
