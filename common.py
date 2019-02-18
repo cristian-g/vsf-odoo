ee08c6c29b93fca466fd7d50dfdb92ea14b44d71
Cristian González <hello@cristiangonzalez.com>
2019-02-18 12:23:16 +0100

    Added common functions used to bake responses

--- common.py
+++ common.py
@@ -1,2 +1,46 @@
 import ast
 import json
+import werkzeug.wrappers
+
+def valid_response(data, status=200):
+    """Valid Response
+    This will be return when the http request was successfully processed."""
+    data = {
+        'count': len(data),
+        'data': data
+    }
+    return werkzeug.wrappers.Response(
+        status=status,
+        content_type='application/json; charset=utf-8',
+        response=json.dumps(data),
+    )
+
+
+def invalid_response(typ, message=None, status=400):
+    """Invalid Response
+    This will be the return value whenever the server runs into an error
+    either from the client or the server."""
+    return werkzeug.wrappers.Response(
+        status=status,
+        content_type='application/json; charset=utf-8',
+        response=json.dumps({
+            'type': typ,
+            'message': message if message else 'wrong arguments (missing validation)',
+        }),
+    )
+
+
+def extract_arguments(payload, offset=0, limit=0, order=None):
+    """."""
+    fields, domain = [], []
+    if payload.get('domain'):
+        domain += ast.literal_eval(payload.get('domain'))
+    if payload.get('fields'):
+        fields += ast.literal_eval(payload.get('fields'))
+    if payload.get('offset'):
+        offset = int(payload['offset'])
+    if payload.get('limit'):
+        limit = int(payload['limit'])
+    if payload.get('order'):
+        order = payload.get('order')
+    return [domain, fields, offset, limit, order]
